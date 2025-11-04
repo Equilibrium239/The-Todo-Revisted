c8cd2f4822aabc26c124e7004a9fdc9152ba5be2
Equilibrium <miltos-05032006@hotmail.com>
2025-11-04 13:20:07 +0100

    added consts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import './style.css'
+
+
+function add(day: string, Time:string) {
+    return day + Time;
+}
+
+
+const btn = document.getElementById("addBtn") as HTMLButtonElement;
+const weekDay = document.getElementById("day") as HTMLInputElement;
+const TimeofDay = document.getElementById("Time") as HTMLInputElement;
+const resultDiv = document.getElementById("result") as HTMLDivElement;
