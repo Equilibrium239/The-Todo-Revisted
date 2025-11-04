69489d66e3c2258bb28cea7e4a3ae6361bed0c76
Equilibrium <miltos-05032006@hotmail.com>
2025-11-04 14:00:40 +0100

    merged

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import './style.css'
 
-
-function add(day: string, Time:string) {
-    return day + Time;
-}
-
-
 const btn = document.getElementById("addBtn") as HTMLButtonElement | null;
 const weekDay = document.getElementById("day") as HTMLInputElement | null;
 const TimeofDay = document.getElementById("Time") as HTMLInputElement | null;
 const resultDiv = document.getElementById("result") as HTMLDivElement | null;
 
 
+const createHtml = (TheTods: Tods[]) => {
+  const todsContainer = document.getElementById("app") as HTMLDivElement;
+
+  if(todsContainer) {
+    todsContainer.innerHTML = "";
+  }
+
+
+}
 
 
 btn?.addEventListener("click", () => {
@@ -20,4 +23,4 @@ btn?.addEventListener("click", () => {
   const WD = String(weekDay.value);
   const ToD = String(TimeofDay.value);
   resultDiv.textContent = String(WD+ToD);
-})
+});
